285581573c5bf4ab2975bf383976aae1f489a332
Alexander Aguilar <aaguila3@villanova.edu>
2018-08-26 16:49:22 -0400

    Made it so you can continuously test

--- src/ReturnContactInfo.java
+++ src/ReturnContactInfo.java
@@ -10,22 +10,32 @@ public class ReturnContactInfo {
 		Scanner scan = new Scanner(System.in);
 		List<String> userInputByLine = new ArrayList<String>();	
 		String userInput = "";
+		Boolean stillTesting = true;
 		
-		while(scan.hasNextLine()) {
-			userInputByLine.add(scan.nextLine());
-			if(userInputByLine.contains("")) {
-				userInputByLine.remove("");
-				break;
+		while(stillTesting) {
+			userInputByLine.clear();
+			userInput = "";
+			while(scan.hasNextLine()) {
+				userInputByLine.add(scan.nextLine());
+				if(userInputByLine.contains("")) {
+					userInputByLine.remove("");
+					break;
+				}
+				
+				if(userInputByLine.contains("Done")) {
+					stillTesting = false;
+					System.out.println("Process Terminated");
+				}
 			}
+			
+			//Putting the user input into a string separated by a new line for each input
+			for(String t: userInputByLine) {
+				userInput += t + "\n";
+			}
+			
+			ContactInfo contactInformation = BusinessCardParser.getContactInfo(userInput);
+			System.out.println(contactInformation.displayInformation());			
 		}
 		scan.close();
-		
-		//Putting the user input into a string separated by a new line for each input
-		for(String t: userInputByLine) {
-			userInput += t + "\n";
-		}
-		
-		ContactInfo contactInformation = BusinessCardParser.getContactInfo(userInput);
-		System.out.println(contactInformation.displayInformation());
 	}
 }
